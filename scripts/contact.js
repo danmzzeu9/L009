b4e61d22f1bdbb499adc3c958afa9f34f77328d0
Daniel Mazzeu <danmzzeu9@gmail.com>
2025-10-31 04:07:37 -0300

    Upload

--- scripts/contact.js
+++ scripts/contact.js
@@ -63,7 +63,7 @@ contactForm.addEventListener('submit', async function(event) {
         };
 
         try {
-            const response = await fetch('http://localhost:3000/apis/sendmail', {
+            const response = await fetch('https://l009-sendmail-production.up.railway.app', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
